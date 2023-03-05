a5a16812c269bb5c0812d3aaec62a0f8eb6e3c3b
olatoCode <awosolat@gmail.com>
2023-03-05 10:31:54 +0100

    refactor: remove unused express methods

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const express = require('express');
 const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
 // Create an array of quotes for each category
 const quotes = {
